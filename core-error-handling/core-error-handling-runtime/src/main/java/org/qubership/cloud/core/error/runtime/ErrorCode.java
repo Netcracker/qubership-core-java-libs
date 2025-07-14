da1a024d5e46a439bb5066adecd7d24a014b1a1d
Evgenii Stetsenko <i@estetsenko.com>
2025-07-14 15:16:15 +0300

    chore: fix some typo

--- core-error-handling/core-error-handling-runtime/src/main/java/org/qubership/cloud/core/error/runtime/ErrorCode.java
+++ core-error-handling/core-error-handling-runtime/src/main/java/org/qubership/cloud/core/error/runtime/ErrorCode.java
@@ -3,7 +3,7 @@ package org.qubership.cloud.core.error.runtime;
 public interface ErrorCode {
     /**
      *
-     * @return error code in the format: [code]-[XХXX], cannot be null
+     * @return error code in the format: [code]-[XXXX], cannot be null
      * where:
      * [code] - Application, Service or Library abbreviation in uppercase with delimiter '-'
      * [XXXX] - digits, padded with zeros on the left (0034, 0140, ...), may be 5 digits long
