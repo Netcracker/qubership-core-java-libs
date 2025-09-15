96bcfbfe6eea043bc6edb7af2f0effe8a65df883
Evgenii Stetsenko <i@estetsenko.com>
2025-09-15 17:23:14 +0300

    Merge pull request #57 from Netcracker/fix/reinit
    fix: call reinitializeRegistry() before test

--- core-context-propagation/framework-contexts/src/test/java/com/netcracker/cloud/framework/contexts/allowedheaders/AllowedHeadersPropertyTest.java
+++ core-context-propagation/framework-contexts/src/test/java/com/netcracker/cloud/framework/contexts/allowedheaders/AllowedHeadersPropertyTest.java
@@ -4,39 +4,28 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import com.netcracker.cloud.context.propagation.core.ContextManager;
 import com.netcracker.cloud.context.propagation.core.RequestContextPropagation;
 import com.netcracker.cloud.framework.contexts.data.ContextDataRequest;
 import com.netcracker.cloud.framework.contexts.data.ContextDataResponse;
 import com.netcracker.cloud.framework.contexts.helper.AbstractContextTestWithProperties;
-import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
-import uk.org.webcompere.systemstubs.jupiter.SystemStub;
-import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
-@ExtendWith(SystemStubsExtension.class)
+import java.util.Map;
+
 class AllowedHeadersPropertyTest extends AbstractContextTestWithProperties {
-    public static final String HEADERS_ENV = "headers_allowed";
-    public static final String HEADERS_PROPERTY = "headers.allowed";
     private static final String CUSTOM_HEADER = "Custom-header-1";
     public static final String ALLOWED_HEADER = "allowed_header";
 
-    private static String headersPropertyBackup;
-
-    @SystemStub
-    private EnvironmentVariables environmentVariables = new EnvironmentVariables(HEADERS_ENV, CUSTOM_HEADER);
+    static Map<String, String> properties = Map.of("headers.allowed", CUSTOM_HEADER);
 
     @BeforeAll
     static void setup() {
-        headersPropertyBackup = System.getProperty(HEADERS_PROPERTY);
-        System.clearProperty(HEADERS_PROPERTY);
+        AbstractContextTestWithProperties.parentSetup(properties);
     }
 
     @AfterAll
     static void tearDown() {
-        if (headersPropertyBackup != null) {
-            System.setProperty("headers.allowed", headersPropertyBackup);
-        }
+        AbstractContextTestWithProperties.parentCleanup(properties);
     }
 
     @Test
