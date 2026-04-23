0cef339f3ac978a6445f11f763d596a969c4ec42
nurtai325 <nurtay.tolymbek@netcracker.com>
2026-04-23 13:05:44 +0500

    chore: refactoring

--- core-utils/k8s/src/main/java/com/netcracker/cloud/security/core/utils/k8s/M2MClientFactory.java
+++ core-utils/k8s/src/main/java/com/netcracker/cloud/security/core/utils/k8s/M2MClientFactory.java
@@ -8,42 +8,39 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 
-import java.util.Base64;
 import java.util.function.Supplier;
 
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class M2MClientFactory {
+
+    private static final Supplier<String> k8sAuthHeaderSupplier =
+            getBearerAuthHeaderSupplier(() -> KubernetesAudienceToken.getToken(AudienceName.NETCRACKER));
+
     public static OkHttpClient getM2MClient(Supplier<String> keycloakTokenSupplier) {
-        return getClient(() -> getBearerAuthHeader(keycloakTokenSupplier));
+        return getClient(new M2MInterceptor(new UrlCache(), getBearerAuthHeaderSupplier(keycloakTokenSupplier), k8sAuthHeaderSupplier));
     }
 
     public static OkHttpClient getDBaaSClient(Supplier<String> keycloakTokenSupplier) {
-        return getClient(() -> getBearerAuthHeader(keycloakTokenSupplier));
+        return getAgentClient(keycloakTokenSupplier, "http://dbaas-agent:8080");
     }
 
     public static OkHttpClient getMaaSClient(Supplier<String> keycloakTokenSupplier) {
-        return getClient(() -> getBearerAuthHeader(keycloakTokenSupplier));
+        return getAgentClient(keycloakTokenSupplier, "http://maas-agent:8080");
     }
 
-    private static OkHttpClient getClient(Supplier<String> fallbackAuthHeaderSupplier) {
-        final OkHttpClient.Builder builder = new OkHttpClient.Builder()
-                .sslSocketFactory(TlsUtils.getSslContext().getSocketFactory(), TlsUtils.getTrustManager());
-        final M2MInterceptor interceptor = new M2MInterceptor(
-                new UrlCache(),
-                fallbackAuthHeaderSupplier,
-                () -> getBearerAuthHeader(() -> KubernetesAudienceToken.getToken(AudienceName.NETCRACKER))
-        );
-        builder.addInterceptor(interceptor);
-        return builder.build();
+    private static OkHttpClient getAgentClient(Supplier<String> keycloakTokenSupplier, String agentUrl) {
+        return getClient(new M2MInterceptor(new UrlCache(), getBearerAuthHeaderSupplier(keycloakTokenSupplier), k8sAuthHeaderSupplier, agentUrl));
     }
 
-    private static String getBearerAuthHeader(Supplier<String> tokenSupplier) {
-        try {
-            return "Bearer "+tokenSupplier.get();
-        } catch (IllegalArgumentException ex) {
-            log.error("Error during kubernetes m2m token acquisition", ex);
-            return null;
-        }
+    private static OkHttpClient getClient(M2MInterceptor interceptor) {
+        return new OkHttpClient.Builder()
+                .sslSocketFactory(TlsUtils.getSslContext().getSocketFactory(), TlsUtils.getTrustManager())
+                .addInterceptor(interceptor)
+                .build();
+    }
+
+    private static Supplier<String> getBearerAuthHeaderSupplier(Supplier<String> tokenSupplier) {
+        return () -> "Bearer " + tokenSupplier.get();
     }
 }
