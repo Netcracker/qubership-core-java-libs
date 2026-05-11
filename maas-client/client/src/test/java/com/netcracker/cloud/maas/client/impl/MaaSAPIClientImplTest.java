380aeac2aa3f3d88690544aff08ee3aaccd833bb
nurtai325 <nurtay.tolymbek@netcracker.com>
2026-05-12 01:24:58 +0500

    chore: refactoring

--- maas-client/client/src/test/java/com/netcracker/cloud/maas/client/impl/MaaSAPIClientImplTest.java
+++ maas-client/client/src/test/java/com/netcracker/cloud/maas/client/impl/MaaSAPIClientImplTest.java
@@ -6,6 +6,8 @@ class MaaSAPIClientImplTest {
     @Test
     void testConstructor() {
         // test that constructor runnable and doesn't throw any exception
+        System.setProperty(Env.PROP_API_URL, "http://localhost:8080");
         new MaaSAPIClientImpl(() -> "faketoken").getRabbitClient();
+        System.clearProperty(Env.PROP_API_URL);
     }
 }
