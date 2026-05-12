f6cd5b2d4d96dd3f313bcbc964e5705dc9c45e69
nurtai325 <nurtay.tolymbek@netcracker.com>
2026-05-13 01:58:00 +0500

    chore: refactoring

--- dbaas-client/dbaas-client-java/dbaas-client-restclient/dbaas-client-webclient/src/main/java/com/netcracker/cloud/dbaas/client/restclient/webclient/DbaasWebClientConfiguration.java
+++ dbaas-client/dbaas-client-java/dbaas-client-restclient/dbaas-client-webclient/src/main/java/com/netcracker/cloud/dbaas/client/restclient/webclient/DbaasWebClientConfiguration.java
@@ -3,10 +3,13 @@ package com.netcracker.cloud.dbaas.client.restclient.webclient;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.netcracker.cloud.restclient.MicroserviceRestClient;
 import com.netcracker.cloud.restclient.webclient.MicroserviceWebClient;
+import com.netcracker.cloud.security.core.auth.M2MManager;
+import com.netcracker.cloud.security.core.utils.k8s.M2MClientFactory;
 import com.netcracker.cloud.smartclient.config.annotation.EnableFrameworkWebClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -19,8 +22,10 @@ import java.util.function.Consumer;
 public class DbaasWebClientConfiguration {
 
     @Bean("dbaasRestClient")
-    public MicroserviceRestClient dbaasRestClient(@Qualifier("m2mWebClient") WebClient webClient) {
-        WebClient customizedWebClient = webClient.mutate()
+    public MicroserviceRestClient dbaasRestClient(M2MManager m2MManager) {
+        var client = M2MClientFactory.getDbaasHttpClient(() -> m2MManager.getToken().getTokenValue());
+        WebClient customizedWebClient = WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(client))
                 .filters(new DisableHttpTraceFilterConsumer())
                 .codecs(clientCodecsConfigurer -> clientCodecsConfigurer.defaultCodecs()
                         .configureDefaultCodec(o -> {
