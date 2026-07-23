f55473d54609afe4881c8cd03193a54dda97d5f1
Alisher Askar <alisherask2005@gmail.com>
2026-07-23 14:00:20 +0500

    chore: take out timeout logic to separate class

--- dbaas-client/dbaas-client-java/dbaas-client-arangodb-starter/src/main/java/com/netcracker/cloud/dbaas/client/arangodb/service/DbaasArangoTemplate.java
+++ dbaas-client/dbaas-client-java/dbaas-client-arangodb-starter/src/main/java/com/netcracker/cloud/dbaas/client/arangodb/service/DbaasArangoTemplate.java
@@ -12,6 +12,7 @@ import com.arangodb.springframework.core.convert.ArangoConverter;
 import com.arangodb.springframework.core.convert.resolver.ResolverFactory;
 import com.arangodb.springframework.core.template.ArangoTemplate;
 import com.netcracker.cloud.dbaas.client.arangodb.configuration.DbaasArangoDBConfigurationProperties;
+import com.netcracker.cloud.dbaas.client.management.ArangoConnectionChecker;
 import com.netcracker.cloud.dbaas.client.management.ArangoDatabaseProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
@@ -20,13 +21,8 @@ import org.springframework.dao.DataAccessException;
 
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Supplier;
-
-import static com.netcracker.cloud.dbaas.client.management.ArangoDatabaseProvider.CHECK_EXECUTOR;
 
 @Slf4j
 public class DbaasArangoTemplate extends ArangoTemplate {
@@ -305,31 +301,17 @@ public class DbaasArangoTemplate extends ArangoTemplate {
     }
 
     protected boolean checkConnection(ArangoOperations operations) {
-        long timeoutMs = dbaasArangoConfig.checkConnectionTimeoutMs();
-        Future<Boolean> future = CHECK_EXECUTOR.submit(() -> {
-            try (ArangoCursor<Integer> query = operations.query("RETURN 42", Integer.class)) {
-                Integer checkValue = query.next();
-                if (checkValue == null || checkValue != 42)
-                    throw new RuntimeException("Wrong check query result: " + checkValue);
-                log.debug("Connection check succeeded, check value: {}", checkValue);
-                return true;
-            }
-        });
-        try {
-            return future.get(timeoutMs, TimeUnit.MILLISECONDS);
-        } catch (TimeoutException e) {
-            future.cancel(true);
-            log.warn("Connection check timed out after {}ms", timeoutMs);
-            return false;
-        } catch (InterruptedException e) {
-            future.cancel(true);
-            Thread.currentThread().interrupt();
-            log.debug("Connection check was interrupted", e);
-            return false;
-        } catch (Exception e) {
-            log.debug("Connection check failed with exception", e);
-            return false;
-        }
+        return ArangoConnectionChecker.check(
+                () -> {
+                    try (ArangoCursor<Integer> query = operations.query("RETURN 42", Integer.class)) {
+                        Integer checkValue = query.next();
+                        if (checkValue == null || checkValue != 42)
+                            throw new RuntimeException("Wrong check query result: " + checkValue);
+                        log.debug("Connection check succeeded, check value: {}", checkValue);
+                        return true;
+                    }
+                },
+                dbaasArangoConfig.checkConnectionTimeoutMs());
     }
 
     protected void initArangoTemplate() {
