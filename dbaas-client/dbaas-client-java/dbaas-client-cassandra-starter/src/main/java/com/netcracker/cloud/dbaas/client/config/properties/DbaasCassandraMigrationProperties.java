a39365da04320f8b22ad76b47ead59b1827e0f1f
Artem Kuleshov <artem.kuleshov@netcracker.com>
2026-08-13 14:16:30 +0500

    fix: make DbaasCassandraMigrationProperties resettable on config refresh

--- dbaas-client/dbaas-client-java/dbaas-client-cassandra-starter/src/main/java/com/netcracker/cloud/dbaas/client/config/properties/DbaasCassandraMigrationProperties.java
+++ dbaas-client/dbaas-client-java/dbaas-client-cassandra-starter/src/main/java/com/netcracker/cloud/dbaas/client/config/properties/DbaasCassandraMigrationProperties.java
@@ -1,51 +1,52 @@
 package com.netcracker.cloud.dbaas.client.config.properties;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import lombok.Data;
 
-@ConfigurationProperties("dbaas.cassandra.migration")
-public record DbaasCassandraMigrationProperties(
-        boolean enabled,
-        String schemaHistoryTableName,
-        VersionProperties version,
-        TemplateProperties template,
-        LockProperties lock,
-        SchemaAgreementProperties schemaAgreement,
-        AmazonKeyspacesProperties amazonKeyspaces
-) {
-    public record VersionProperties(
-            String settingsResourcePath,
-            String directoryPath,
-            String resourceNamePattern
-    ) {
+@Data
+public class DbaasCassandraMigrationProperties {
+    private boolean enabled;
+    private String schemaHistoryTableName;
+    private VersionProperties version;
+    private TemplateProperties template;
+    private LockProperties lock;
+    private SchemaAgreementProperties schemaAgreement;
+    private AmazonKeyspacesProperties amazonKeyspaces;
+
+    @Data
+    public static class VersionProperties {
+        private String settingsResourcePath;
+        private String directoryPath;
+        private String resourceNamePattern;
     }
 
-    public record TemplateProperties(
-            String definitionsResourcePath
-    ) {
+    @Data
+    public static class TemplateProperties {
+        private String definitionsResourcePath;
     }
 
-    public record LockProperties(
-            String tableName,
-            Long retryDelay,
-            Long lockLifetime,
-            Long extensionPeriod,
-            Long extensionFailRetryDelay
-    ) {
+    @Data
+    public static class LockProperties {
+        private String tableName;
+        private Long retryDelay;
+        private Long lockLifetime;
+        private Long extensionPeriod;
+        private Long extensionFailRetryDelay;
     }
 
-    public record SchemaAgreementProperties(
-            Long awaitRetryDelay
-    ) {
+    @Data
+    public static class SchemaAgreementProperties {
+        private Long awaitRetryDelay;
     }
 
-    public record AmazonKeyspacesProperties(
-            boolean enabled,
-            TableStatusCheck tableStatusCheck
-    ) {
-        public record TableStatusCheck(
-                Long preDelay,
-                Long retryDelay
-        ) {
+    @Data
+    public static class AmazonKeyspacesProperties {
+        private boolean enabled;
+        private TableStatusCheck tableStatusCheck;
+
+        @Data
+        public static class TableStatusCheck {
+            private Long preDelay;
+            private Long retryDelay;
         }
     }
 }
